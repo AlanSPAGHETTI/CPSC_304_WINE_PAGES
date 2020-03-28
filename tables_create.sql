2fc4da0105711aadd892ecc7c094a2414f667d2a
AlanSPAGHETTI <Alanlongsecure@protonmail.com>
2020-03-27 17:31:42 -0700

    created SQL for CUSTOMER table

---
+++ tables_create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE CUSTOMER(
+    CustomerID      INT          PRIMARY KEY AUTO_INCREMENT,
+    CustomerName    VARCHAR(255) NOT NULL,
+    CustomerAddress VARCHAR(255) NOT NULL,
+    CustomerAge     INT          NOT NULL
+);
