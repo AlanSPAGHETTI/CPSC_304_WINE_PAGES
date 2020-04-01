e3995ec20adcae844ca3c8588e42d5ff18ba9398
AlanSPAGHETTI <Alanlongsecure@protonmail.com>
2020-03-31 20:36:14 -0700

    Create tables_delete.sql

---
+++ tables_delete.sql
@@ -0,0 +1,28 @@
+# The following commands handles deletion of primary tables
+DROP TABLE CUSTOMER;
+
+DROP TABLE GRAPE;
+
+DROP TABLE STORAGE;
+
+DROP TABLE QUALITY;
+
+DROP TABLE WINERY;
+
+# The following commands handles deletion of secondary tables
+DROP TABLE WINERY_REP;
+
+DROP TABLE WINERY_BRANCH;
+
+# The following commands handles deletion of tertiary tables
+DROP TABLE FAVORITE;
+
+DROP TABLE WINE;
+
+DROP TABLE WINE_BROWSED_BY;
+
+DROP TABLE WINE_MADE_FROM;
+
+DROP TABLE WINE_STORED_AT;
+
+DROP TABLE WINE_HAS_QUALITY;
