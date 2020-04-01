60e48299a3bf8f74f9b8952c34fb26edaaee7c8e
AlanSPAGHETTI <Alanlongsecure@protonmail.com>
2020-03-31 21:20:57 -0700

    Create tables_insert.sql

---
+++ tables_insert.sql
@@ -0,0 +1,32 @@
+# The following commands handles insertion of primary tables
+INSERT INTO CUSTOMER
+VALUES (1, "Chloe Brown",     "2048 Adelaide St, Toronto",        25);
+
+INSERT INTO CUSTOMER
+VALUES (2, "Jessica Jones",   "3018 Roger Street, Port Alberni",  32);
+
+INSERT INTO CUSTOMER
+VALUES (3, "Sophie Martinez", "1795 Victoria Park Ave, Toronto",  27);
+
+INSERT INTO CUSTOMER
+VALUES (4, "James P Downey",  "4832 Tchesinkut Lake Rd, Chicago", 45);
+
+INSERT INTO CUSTOMER
+VALUES (5, "Anton S Newton",  "691 Haaglund Rd, Texas",           65);
+
+
+
+INSERT INTO GRAPE
+VALUES (1, "Gewurztraminer", "Large",  "Red");
+
+INSERT INTO GRAPE
+VALUES (2, "Pinot Noir",     "Large",  "Purple");
+
+INSERT INTO GRAPE
+VALUES (3, "Chardonnay",     "Medium", "Green");
+
+INSERT INTO GRAPE
+VALUES (4, "Zinfandel",      "Medium", "Black");
+
+INSERT INTO GRAPE
+VALUES (5, "Riesling",       "Small",  "Green");
