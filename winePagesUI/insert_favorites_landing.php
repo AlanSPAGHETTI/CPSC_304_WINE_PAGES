9ab302a40ad5bec512b8178b0f291799697cb37a
MacIverson <49001044+MacIverson@users.noreply.github.com>
2020-04-04 18:48:36 -0700

    Create insert_favorites_landing

---
+++ winePagesUI/insert_favorites_landing.php
@@ -0,0 +1,50 @@
+<?php
+include_once 'includes/connect.php';
+$conn = OpenCon();
+$sql = "SELECT * FROM favorite, wine, customer WHERE favorite.WineID = wine.WineID AND customer.CustomerID = favorite.CustomerID;";
+$result = mysqli_query($conn, $sql);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>The Wine Pages: Favourites</title>
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="winePagesUI.css">
+    <link rel="icon" type="image/ico" href="logo.png" />
+</head>
+<body>
+<div class="header">
+    <p><span>Wine / Pages <span>| Admin Page</span><span><img src="logo.png"></p>
+</div>
+
+<div class="topnav">
+    <a class="active" href="index.php">Home</a>
+    <form class="searchbox" id="searchbox" action="searchbox_results.php" method="post">
+        <input name="search_keyword" type="text" placeholder="Browse inventory...">
+        <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+    <a href="uiRegistration.php">Manage Users</a>
+    <a href="uiFavourite.php">Manage Favorites</a>
+    <a href="uiInventory.php">Inventory Stats</a>
+    <a href="uiQuickAccess.php">Quick Access Panel</a>
+</div>
+
+<h1>Add a favorite for a customer!</h1>
+<form class="Search2" id="Favorite" action="add_favorite.php" method="post">
+    <div>
+        <label for="CustomerID">Customer ID:</label>
+        <input type="number" name="CustId" required>
+    </div>
+
+    <div>
+        <label for="WineID">Wine ID:</label>
+        <input type="number" name="WineName" required>
+    </div>
+
+    <button>Submit</button>
+</form>
+
+</body>
+</html>
